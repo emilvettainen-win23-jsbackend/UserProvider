cb8fc9ac347ab90b20e4564edb6a1db7708f6ccd
emil-vettainen <emil.vettainen@utb.ecutbildning.se>
2024-05-05 23:17:36 +0200

    init

---
+++ UserProvider/Data/Entities/UserEntity.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserProvider.Data.Entities;
+
+public class UserEntity : IdentityUser
+{
+    [ProtectedPersonalData]
+    [Required]
+    [Column(TypeName = "nvarchar(50)")]
+    public string FirstName { get; set; } = null!;
+
+    [ProtectedPersonalData]
+    [Required]
+    [Column(TypeName = "nvarchar(50)")]
+    public string LastName { get; set; } = null!;
+
+    public string? Biography { get; set; }
+    public string? ProfileImageUrl { get; set; }
+
+    [Required]
+    [Column(TypeName = "datetime2")]
+    public DateTime Created { get; set; }
+
+    [Required]
+    [Column(TypeName = "datetime2")]
+    public DateTime Modified { get; set; }
+
+    public bool IsExternalAccount { get; set; } = false;
+}
